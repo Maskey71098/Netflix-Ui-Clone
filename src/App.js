43ea5aa830fb554b1c4294084ad3f8e25372791c
Maskey71098 <abhishekmaskey71098@gmail.com>
2021-06-12 22:08:31 +0545

    implemented more rows and fixed css rulings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import requests from './requests';
 import Row from './Row.js';
+import Banner from './Banner.js';
 
 //3d78b4595430d1139de9e2c81172036d  -- api key
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Ui clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      {/* Navbar */}
+      {/* Bannerr */}
+      <Banner />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
